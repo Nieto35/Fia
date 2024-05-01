1ad564aaf4818988a1416fd1f8dce31a76ba8e99
Nieto35 <jfnieto00@misena.edu.co>
2024-05-01 16:49:31 +0200

    correction errors

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react'
 
-export function useLocalStorage (key, initialValue) {
+export function useLocalStorage (key: any, initialValue: any) {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
@@ -18,7 +18,7 @@ export function useLocalStorage (key, initialValue) {
 
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to localStorage.
-  const setValue = value => {
+  const setValue = (value: ((val: typeof storedValue) => typeof storedValue) | typeof storedValue)  => {
     try {
       // Allow value to be a function so we have same API as useState
       const valueToStore =
